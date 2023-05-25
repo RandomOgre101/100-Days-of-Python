aef23a2f60f0adcc10338a83a42e446b204564bb
RandomOgre101 <daxsteel1234@gmail.com>
2023-05-25 10:48:29 +0530

    day 30

--- Day 26 - List Comprehension and NATO/NATO-alphabet-start/main.py
+++ Day 26 - List Comprehension and NATO/NATO-alphabet-start/main.py
@@ -10,8 +10,16 @@ for (index, row) in df.iterrows():
     nato[row.letter] = row.code
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter the word: ").upper()
-user_list = [letter for letter in user_input]
+def generate():
+    user_input = input("Enter the word: ").upper()
+    try:
+        user_list = [letter for letter in user_input]
+        nato_list = [nato[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet and no spaces please!")
+        generate()
+    else:
+        print(nato_list)
 
-nato_list = [nato[letter] for letter in user_input]
-print(nato_list)
+
+generate()
