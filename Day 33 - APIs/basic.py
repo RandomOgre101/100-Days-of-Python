cdcdf448bf4901d5375052f6da7e806dd42e0e72
rishikeshavravi2020 <daxsteel1234@gmail.com>
2023-05-28 17:15:21 +0530

    day 33

---
+++ Day 33 - APIs/basic.py
@@ -0,0 +1,36 @@
+import requests
+import datetime as dt
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+# data = response.json()
+
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+
+# iss_position = (longitude, latitude)
+# print(iss_position)
+
+LAT = 13.082680
+LONG = 80.270721
+
+parameters = {
+    "lat": LAT,
+    "lng": LONG,
+    "formatted": 0,
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+time_now = dt.datetime.now()
+print(time_now.hour)
+
+
+
+
+
