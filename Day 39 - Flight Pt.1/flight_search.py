4b21ef971ee1453ce4d510c65fd65971910d0455
Rishikeshav Ravichandran <94991501+RandomOgre101@users.noreply.github.com>
2023-06-08 12:22:30 +0530

    Update flight_search.py

--- Day 39 - Flight Pt.1/flight_search.py
+++ Day 39 - Flight Pt.1/flight_search.py
@@ -1,8 +1,8 @@
 import requests
 from flight_data import FlightData
 
-TEQUILA_ENDPOINT = "https://api.tequila.kiwi.com"
-TEQUILA_KEY = "N21c0-INfwr5pVycTrR-5ebzDkFXmuEa"
+TEQUILA_ENDPOINT = ""
+TEQUILA_KEY = ""
 
 
 class FlightSearch:
