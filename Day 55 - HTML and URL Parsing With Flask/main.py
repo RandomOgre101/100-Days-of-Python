98b3d7dc060caa2622bcf64f896d194b4d1d27bb
rishikeshavravi2020 <daxsteel1234@gmail.com>
2023-07-08 12:34:46 +0530

    day 55

--- Day 55 - HTML and URL Parsing With Flask/main.py
+++ Day 55 - HTML and URL Parsing With Flask/main.py
@@ -14,27 +14,31 @@ def make_underlined(function):
         return f"<u>{function()}</u>"
     return wrapper 
 
+colors = ['blue', 'red', 'purple', 'green', 'yellow', 'pink']
 
 from flask import Flask
+import random
 
 app = Flask(__name__)
+
+crct = random.randint(0,9)
 
 @app.route('/')
 @make_bold
 def landing_page():
-    return '<h1 styles="color:blue">Choose a number betwen 0 and 9</h1>'\
+    return f'<h1 style="color:{random.choice(colors)}">Choose a number betwen 0 and 9</h1>'\
             '<img src="https://media.giphy.com/media/oBQZIgNobc7ewVWvCd/giphy-downsized-large.gif">'
 
 @app.route('/<int:num>')
 def number(num):
-    if num == 6:
-        return  '<h1>CORRECT!</h1>'\
+    if num == crct:
+        return  f'<h1 style="color:{random.choice(colors)}">CORRECT!</h1>'\
                 '<img src="https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif">'
-    elif num > 6:
-        return '<h1>Too high! Guess again</h1>'\
+    elif num > crct:
+        return f'<h1 style="color:{random.choice(colors)}">Too high! Guess again</h1>'\
                 '<img src="https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif">'
     else:
-        return '<h1>Too low! Guess again</h1>'\
+        return f'<h1 style="color:{random.choice(colors)}">Too low! Guess again</h1>'\
                 '<img src=" https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif">'
 
 
